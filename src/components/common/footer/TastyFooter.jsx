9c0f377c2f72d7cac3e328f33eeb255baddde1b6
Rocío Pereyra <rociopereyrax@gmail.com>
2021-09-08 21:10:54 -0600

    Se agregaron iconos al footer y se lo fijó en la parte inferior

--- src/components/common/footer/TastyFooter.jsx
+++ src/components/common/footer/TastyFooter.jsx
@@ -18,13 +18,13 @@ export const TastyFooter = () => {
             className="col-12 col-md-4 mb-4 mb-md-2 d-flex align-items-center justify-content-center"
           >
             <a href="#" className=" redes"
-              ><i className="mx-2" aria-hidden="true"><IoLogoFacebook /></i
+              ><i className="mx-2" aria-hidden="true"><IoLogoFacebook style={{ fontSize: 40 }}/></i
             ></a>
             <a href="#" className=" redes"
-              ><i className="mx-2" aria-hidden="true"><AiOutlineInstagram /></i
+              ><i className="mx-2" aria-hidden="true"><AiOutlineInstagram style={{ fontSize: 40 }}/></i
             ></a>
             <a href="#" className=" redes"
-              ><i className="mx-2" aria-hidden="true"><AiFillTwitterCircle /></i
+              ><i className="mx-2" aria-hidden="true"><AiFillTwitterCircle style={{ fontSize: 40 }}/></i
             ></a>
           </div>
           <div
