17a9642358d3aac2e12d9a72f5dae133845e1ccb
pablogiroud <pablogiroud@gmail.com>
2021-09-17 02:16:30 -0300

    fix cocina (un console log rompia todo historicamente)

--- src/pages/Cocina.jsx
+++ src/pages/Cocina.jsx
@@ -1,35 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { getComandas } from '../helpers/comandasCocina'
 import CardCocina from '../components/CardCocina'
-import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap'
 
 const Cocina = () => {
-  const [comandas, setComandas] = useState([]);
-  const [comandasCocina, setComandasCocina] =useState([])
+  const [comandas, setComandas] = useState([])
+  const [comandasCocina, setComandasCocina] = useState([])
 
   useEffect(() => {
     getComandas().then((respuesta) => {
-      setComandas(respuesta.comanda);
-      console.log(comandas)
-    });
-  }, []);
+      setComandas(respuesta.comanda)
+    })
+  }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     let comandaCocina = comandas.filter((comanda) => {
-      return comanda.tipo === "Plato";
-    });
-    setComandasCocina(comandaCocina);
-    console.log(comandaCocina);
-  }, []);
-  
+      return comanda.tipo === 'Plato'
+    })
+    setComandasCocina(comandaCocina)
+  }, [])
+
   return (
     <>
       <Container fluid className="mt-2">
         <CardCocina comandas={comandasCocina} />
       </Container>
     </>
-  );
-};
-
+  )
+}
 
 export default Cocina
